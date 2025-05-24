a855f8023a6fee567be302bd186b0b997e8af681
failsplat <68579200+failsplat@users.noreply.github.com>
2025-05-24 15:25:14 -0400

    v2.7.1
    (Fix) Swap Icy/Normal Temples of Aegir

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Reus2Surveyor
         /// </summary>
         /// 
 
-        public static Version programVersion = new(2, 7, 0);
+        public static Version programVersion = new(2, 7, 1);
 
         [STAThread]
         static void Main()
