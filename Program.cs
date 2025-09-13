9b10bca3d8eda8f9602aaaa9ac5451b8f1cc7a63
failsplat <68579200+failsplat@users.noreply.github.com>
2025-09-13 02:53:53 -0400

    v2.17.0 Kruger
    Added new biotica and era for the Kruger update
    Biotica availability changed to only count which biotica can be drafted based on the current giants, instead of where they can placed (but not unlocked outside of Evolve unlocks)
    Biotica affected: Jet, Glacial Erratics, Kettle Lake, Smokey Quartz, Black Mangrove, Sea Horse, Balloonfish, Barracuda

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Reus2Surveyor
         /// </summary>
         /// 
 
-        public static Version programVersion = new(2, 16, 1);
+        public static Version programVersion = new(2, 17, 0);
 
         [STAThread]
         static void Main()
