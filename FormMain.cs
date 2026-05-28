16a5fb36f8ba802bb682c5592f24ff490e849a75
failsplat <68579200+failsplat@users.noreply.github.com>
2026-05-27 21:08:21 -0400

    Obscure UserProfile

--- FormMain.cs
+++ FormMain.cs
@@ -89,7 +89,7 @@ namespace Reus2Surveyor
             if (Path.GetFileName(value).StartsWith("profile_"))
             {
                 this.ProfileDir = value;
-                this.profileFolderTextBox.Text = this.ProfileDir;
+                this.profileFolderTextBox.Text = this.ProfileDir.Replace(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "%USERPROFILE%");
                 this.profileFolderTextBox.BackColor = this.profileFolderTextBox.BackColor;
                 this.profileFolderTextBox.ForeColor = System.Drawing.Color.Green;
                 this.profileDirOK = true;
@@ -101,7 +101,7 @@ namespace Reus2Surveyor
             else
             {
                 this.ProfileDir = value;
-                this.profileFolderTextBox.Text = this.ProfileDir;
+                this.profileFolderTextBox.Text = this.ProfileDir.Replace(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "%USERPROFILE%");
                 this.profileFolderTextBox.BackColor = this.profileFolderTextBox.BackColor;
                 this.profileFolderTextBox.ForeColor = System.Drawing.Color.Red;
                 this.profileDirOK = false;
